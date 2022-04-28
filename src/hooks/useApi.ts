cc4a37b1bd5418be9d55c7d863b042986a9a59ff
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-04-28 13:40:50 -0300

    teste r

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -6,16 +6,29 @@ const api = axios.create({
 
 export const useApi = () => ({
     validateToken: async (token: string) => {
+
+        return {
+            user: {
+                id: 3, name: 'luiz', email: 'luiz@gmail.com'
+            }
+            
+        }
         const response = await api.post('/validate', { token });
         return response.data;
     },
     signin: async (email: string, password: string) => {
-
+        return {
+            user: {
+                id: 3, name: 'luiz', email: 'luiz@gmail.com'
+            },
+            token: '123456789'
+        }
         const response = await api.post('/signin', {email, password });
         return response.data;
 
     },
     logout: async () => {
+        return { status: true};
         const response = await api.post('/logout');
         return response.data;
     }
